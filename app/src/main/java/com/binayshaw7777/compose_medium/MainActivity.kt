df36125182fd8a9f023912a7f0726323aaa79e69
Binay Shaw <binayshaw7777@gmail.com>
2024-01-18 07:56:06 +0530

    Sample Code Done

--- app/src/main/java/com/binayshaw7777/compose_medium/MainActivity.kt
+++ app/src/main/java/com/binayshaw7777/compose_medium/MainActivity.kt
@@ -3,13 +3,23 @@ package com.binayshaw7777.compose_medium
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.binayshaw7777.compose_medium.ui.theme.ComposeMediumTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -30,17 +40,67 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting() {
+    Column(
+        Modifier.padding(8.dp)
+    ) {
+
+        Text(
+            fontSize = 14.sp,
+            text = "Buttons with Arrangement.SpaceEvenly",
+            modifier = Modifier.padding(top = 20.dp, bottom = 10.dp)
+        )
+
+        Row(
+            Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Button(onClick = {
+                // Do something
+            }) {
+                Text(text = "Button")
+            }
+            Button(onClick = {
+                // Do something
+            }) {
+                Text(text = "Second Button")
+            }
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(
+            fontSize = 14.sp,
+            text = "Buttons with 1f Weight & 16dp Horizontal Padding",
+            modifier = Modifier.padding(top = 20.dp, bottom = 10.dp)
+        )
+
+        Row(
+            Modifier.fillMaxWidth(),
+        ) {
+            Button(modifier = Modifier
+                .weight(1f)
+                .padding(horizontal = 16.dp), onClick = {
+                // Do something
+            }) {
+                Text(text = "Button")
+            }
+
+            Button(modifier = Modifier
+                .weight(2f)
+                .padding(horizontal = 16.dp), onClick = {
+                // Do something
+            }) {
+                Text(text = "Second Button")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeMediumTheme {
-        Greeting("Android")
+        Greeting()
     }
 }

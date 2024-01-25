1667777183cd65c93673784370510a84f637d7e4
Binay Shaw <binayshaw7777@gmail.com>
2024-01-25 09:29:49 +0530

    SubComposeAsyncImage example

--- app/src/main/java/com/binayshaw7777/compose_medium/MainActivity.kt
+++ app/src/main/java/com/binayshaw7777/compose_medium/MainActivity.kt
@@ -3,16 +3,34 @@ package com.binayshaw7777.compose_medium
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.SubcomposeAsyncImage
+import coil.request.ImageRequest
 import com.binayshaw7777.compose_medium.ui.theme.ComposeMediumTheme
 
 class MainActivity : ComponentActivity() {
+
+    val imageUrl = "https://i.pinimg.com/originals/3d/f4/2a/3df42a95e75f62ee4768c256fff25c39.jpg"
+    val brokenImageUrl = "https://i.pinimg.com/originals/3d/f4/2a/3df42a95e755c.jpg"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -20,27 +38,53 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color.Blue.copy(alpha = 0.01f)
                 ) {
-                    Greeting("Android")
+                    Column(
+                        Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.SpaceAround
+                    ) {
+                        SubcomposeAsyncImage(
+                            modifier = Modifier
+                                .size(120.dp)
+                                .clip(CircleShape),
+                            model = ImageRequest.Builder(LocalContext.current)
+                                .data(imageUrl)
+                                .error(R.drawable.error_placeholder)
+                                .crossfade(true)
+                                .build(),
+                            loading = {
+                                CircularProgressIndicator(
+                                    color = Color.Blue,
+                                    modifier = Modifier.padding(36.dp)
+                                )
+                            },
+                            contentDescription = null,
+                            contentScale = ContentScale.Crop
+                        )
+
+                        SubcomposeAsyncImage(
+                            modifier = Modifier
+                                .size(120.dp)
+                                .clip(CircleShape),
+                            model = ImageRequest.Builder(LocalContext.current)
+                                .data(brokenImageUrl)
+                                .error(R.drawable.error_placeholder)
+                                .crossfade(true)
+                                .build(),
+                            loading = {
+                                CircularProgressIndicator(
+                                    color = Color.Blue,
+                                    modifier = Modifier.padding(36.dp)
+                                )
+                            },
+                            contentDescription = null,
+                            contentScale = ContentScale.Crop
+                        )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeMediumTheme {
-        Greeting("Android")
     }
 }

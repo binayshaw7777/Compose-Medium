4dbd53d717d9157e5d8698b42569b6e4a6fa0955
binay shaw <binayshaw7777@gmail.com>
2024-03-12 14:01:22 +0530

    Inherit Styles

--- app/src/main/java/com/binayshaw7777/compose_medium/MainActivity.kt
+++ app/src/main/java/com/binayshaw7777/compose_medium/MainActivity.kt
@@ -3,14 +3,20 @@ package com.binayshaw7777.compose_medium
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.binayshaw7777.compose_medium.ui.theme.ComposeMediumTheme
+import com.binayshaw7777.compose_medium.ui.theme.myTextStyle
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -30,17 +36,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting() {
+    Column(modifier = Modifier.padding(20.dp)) {
+
+        Text(
+            text = "Heading",
+            style = myTextStyle
+        )
+
+        Text(text = "Sub-heading",
+            style = myTextStyle.copy(
+                color = Color.Gray,
+                letterSpacing = 2.sp
+            )
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeMediumTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
--- app/src/main/java/com/binayshaw7777/compose_medium/ui/theme/Type.kt
+++ app/src/main/java/com/binayshaw7777/compose_medium/ui/theme/Type.kt
@@ -1,6 +1,7 @@
 package com.binayshaw7777.compose_medium.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -31,4 +32,9 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+
+val myTextStyle = TextStyle(
+    fontWeight = FontWeight.Medium,
+    color = Color.Black
 )
